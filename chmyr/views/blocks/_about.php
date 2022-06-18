954238224fd51645dc8071f4cb6fe9342df6712d
Tambovskii <tambik1@gmail.com>
2022-06-18 16:26:45 +0200

    Проработка верстки layout и about

--- chmyr/views/blocks/_about.php
+++ chmyr/views/blocks/_about.php
@@ -1,1 +1,4 @@
-<h1>About</h1>
+<div class="content_container">
+	<h1 class="about_title">О проекте</h1>
+	<p class="about_description">Этот проект был создан для того, что бы я показал, что умею разрабатывать web-приложения</p>
+</div>
--- chmyr/views/layout/main.php
+++ chmyr/views/layout/main.php
@@ -16,10 +16,37 @@
 	<link rel="stylesheet" href="/public/css/style.css">
 </head>
 <body>
-<div class="content">
-
-	<?= $content ?>
-</div>
+	<header>
+		<div class="header_logo">
+			<a href="/"><img class="header_logo__img" src="/public/img/layout_img/chmyr_logo.png" alt="logo"></a>
+		</div>
+		<div class="header_menu">
+<!--			TODO - Подумать, стоит ли вынести пункты меню в конфиг и выводить динамически-->
+			<div class="header_menu_points">
+				<a class="header_menu_points__point" href="#">Главная</a>
+				<a class="header_menu_points__point" href="#">Новое</a>
+				<a class="header_menu_points__point" href="#">Избранное</a>
+			</div>
+		</div>
+		<div class="header_menu_search">
+			<form class="header_menu_search__form" action="#" method="get">
+				<input class="header_menu_search__input" placeholder="Искать здесь..." type="search">
+				<button class="header_menu_search__button" type="submit"></button>
+			</form>
+		</div>
+		<div class="header_user_menu">
+			<div class="header_user_menu_photo">
+				<a href="#"><img class="header_user_menu_photo__img" src="/public/img/layout_img/user_photo__test.jpg" alt="user-photo"></a>
+			</div>
+			<div class="header_user_menu_fio">
+				<a class="header_user_menu_fio_name__a" href="#"><span class="header_user_menu_fio_name">Вася</span></a>
+				<a class="header_user_menu_fio_surname__a" href="#"><span class="header_user_menu_fio_surname">Пупкин</span></a>
+			</div>
+		</div>
+	</header>
+	<div class="content">
+		<?= $content ?>
+	</div>
 
 </body>
 </html>
